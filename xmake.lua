a5ac917ae3259ca5428c3551c89eb2747134c347
Kerolt <1517482303@qq.com>
2024-04-18 18:39:42 +0800

    build: Modify the target build location and add dependencies for json parsing

--- xmake.lua
+++ xmake.lua
@@ -2,6 +2,8 @@ add_rules("mode.release", "mode.debug")
 
 set_languages("c++17")
 
+add_requires("nlohmann_json")
+
 add_includedirs("src/buffer")
 add_includedirs("src/log")
 add_includedirs("src/timer")
@@ -10,7 +12,10 @@ add_includedirs("src/server")
 add_includedirs("src/http")
 
 target("server")
+    set_targetdir("./")
     set_kind("binary")
     add_files("src/**.cc")
+    add_links("mysqlclient")
+    add_packages("nlohmann_json")
 
 includes("test")
